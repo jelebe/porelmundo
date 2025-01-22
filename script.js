e3a63170bd74f681ab086088ef306434992215b0
jelebe <48688326+jelebe@users.noreply.github.com>
2025-01-22 13:23:46 +0100

    Update styles.css

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCLHKZmeUUahOD9pCG9HGRed9zxwP5vHb0",
+  authDomain: "besosporelmundo.firebaseapp.com",
+  projectId: "besosporelmundo",
+  storageBucket: "besosporelmundo.firebasestorage.app",
+  messagingSenderId: "716617534132",
+  appId: "1:716617534132:web:0a28680cf49aa1fcdd25e1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firestore
+var db = firebase.firestore();
+
 var map = L.map('map').setView([0, 0], 2);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -145,60 +164,4 @@ function handleFileSelect(event, marker) {
 
 function readFile(file, callback) {
   var reader = new FileReader();
-  reader.onload = function(event) {
-    callback(event.target.result);
-  };
-  reader.readAsDataURL(file);
-}
-
-window.editMarker = function(markerId) {
-  var marker = map._layers[markerId];
-  if (marker) {
-    openModal(marker.getLatLng(), marker, true);
-  }
-};
-
-function removeMarker(marker) {
-  map.removeLayer(marker);
-  saveMarkers();
-}
-
-function updateMarkerPopup(latlng, marker) {
-  L.popup()
-    .setLatLng(latlng)
-    .setContent((marker.date ? '<p class="marker-date">' + marker.date + '</p><br>' : '') +
-                (marker.image ? '<img src="' + marker.image + '" alt="Imagen" width="200"><br>' : '') +
-                (marker.description ? '<p>' + marker.description + '</p><br>' : '') +
-                '<button class="edit-marker-btn" onclick="editMarker(' + marker._leaflet_id + ')">✎</button>' +
-                '<button class="delete-marker-btn" onclick="deleteMarker(' + marker._leaflet_id + ')">✖</button>')
-    .openOn(map);
-}
-
-function hasContent(marker) {
-  return marker.image || marker.description || marker.date;
-}
-
-function saveMarkers() {
-  var markers = [];
-  map.eachLayer(function(layer) {
-    if (layer instanceof L.Marker) {
-      markers.push({
-        latlng: layer.getLatLng(),
-        image: layer.image,
-        description: layer.description,
-        date: layer.date
-      });
-    }
-  });
-  localStorage.setItem('markers', JSON.stringify(markers));
-}
-
-function loadMarkers() {
-  var markers = JSON.parse(localStorage.getItem('markers')) || [];
-  markers.forEach(function(markerData) {
-    addMarker(markerData.latlng, markerData.image, markerData.description, markerData.date);
-  });
-}
-
-loadMarkers();
-
+ [_{{{CITATION{{{_1{](https://github.com/la9una/web/tree/ba1073ae044ebb7b538a3b13f0f9598f7c410bb6/docs%2Fbootstrap%2Falignci.md)[_{{{CITATION{{{_2{](https://github.com/Alefrankie/Miranda/tree/8e6197eb99daa5f79524b0363e52c7b6d5e34c9b/app%2Fviews%2Ftemplates%2Fusuarios%2Flogin.php)[_{{{CITATION{{{_3{](https://github.com/angeldavid06/gestor_de_archivos/tree/ca6e8bac430129589557a48e7f74fd91d9143739/index.php)[_{{{CITATION{{{_4{](https://github.com/SeanCena123/test-project-viola/tree/66037dd59ce3ad042e431600ef674f0cc66797d8/public%2Fjs%2Fsignin.js)[_{{{CITATION{{{_5{](https://github.com/alyzadiaz/medella/tree/db61480eb264d0869bec504687e228535076b457/HTML-CSS%2Fsearch.php)[_{{{CITATION{{{_6{](https://github.com/alyzadiaz/medella/tree/db61480eb264d0869bec504687e228535076b457/HTML-CSS%2Fhome.php)[_{{{CITATION{{{_7{](https://github.com/izaakbc/izaakbc.github.io/tree/b028145610b876ca22b983d8c24f653950cb4b1d/LeafletWMS.js)[_{{{CITATION{{{_8{](https://github.com/CarlosFTG/weatherapp/tree/05f5b98653e767c14fa9c50ae51ee620133b5c64/front%2Fsrc%2Fapp%2Fcomponents%2Fmap%2Fmap.component.ts)
