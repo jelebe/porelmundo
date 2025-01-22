dea2b443003a83e726221b874995f1004129f99e
jelebe <48688326+jelebe@users.noreply.github.com>
2025-01-22 16:59:45 +0100

    cambios estructurales

---
+++ src/login.js
@@ -0,0 +1,13 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Aquí puedes agregar la lógica de autenticación
+    if (username === 'usuario' && password === 'contraseña') {
+        // Redirigir a la página con el mapa
+        window.location.href = 'mapa.html';
+    } else {
+        document.getElementById('error-message').textContent = 'Usuario o contraseña incorrectos';
+    }
+});
